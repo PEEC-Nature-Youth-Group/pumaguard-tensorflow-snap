2683c6739099fd5f7666ed3c3075c692433c7d57
Nicolas Bock <nicolasbock@gmail.com>
2025-03-13 20:40:21 -0600

    Try to connect to TPU or GPU for diagnostics
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- pumaguard_tensorflow/main.py
+++ pumaguard_tensorflow/main.py
@@ -2,7 +2,7 @@
 Main module for the pumaguard_tensorflow package.
 """
 
-import tensorflow
+import tensorflow as tf
 
 from pumaguard_tensorflow import __VERSION__
 
@@ -12,4 +12,19 @@ def main():
     Entry point for the pumaguard_tensorflow application.
     """
     print(f'pumaguard-tensorflow version {__VERSION__}')
-    print(f'TensorFlow version {tensorflow.__version__}')
+    print(f'TensorFlow version {tf.__version__}')
+    print('Trying to connect to a TPU')
+    try:
+        tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
+        tf.config.experimental_connect_to_cluster(tpu)
+        tf.tpu.experimental.initialize_tpu_system(tpu)
+        print('Running on a TPU with %d cores',
+              tpu.num_accelerators()["TPU"])
+    except ValueError:
+        print("WARNING: Not connected to a TPU runtime; Will try GPU")
+        if tf.config.list_physical_devices('GPU'):
+            print('Running on %d GPUs', len(
+                tf.config.list_physical_devices("GPU")))
+        print('WARNING: Not connected to TPU or GPU runtime; '
+              'Will use CPU context')
+    print('Done!')
